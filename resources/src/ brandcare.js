224cb21a3cfbf9011e6672786e6c17aeda8e1f4e
Slobodan <mr.slobodan@hotmail.rs>
2020-09-29 23:46:48 +0200

    project fix

---
+++ resources/src/ brandcare.js
@@ -0,0 +1,7 @@
+const discription = document.querySelector('.discription')
+const imgContainer = document.querySelector('.img-container')
+
+
+const img = `<img src="resources/images/bcdistribution.png" width="50%"/>`
+
+imgContainer.append(img)
